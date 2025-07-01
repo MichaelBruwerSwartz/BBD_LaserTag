eb7bbdf7f5675d5243d0879a2507fa82896fc299
Cable <cableplays1912@gmail.com>
2025-07-01 14:19:23 +0200

    Update PlayerLobby.js

--- client/src/pages/PlayerLobby.js
+++ client/src/pages/PlayerLobby.js
@@ -19,7 +19,7 @@ export default function PlayerLobby() {
     console.log(username);
 
     const socket = new WebSocket(
-      `wss://bbd-lasertag.onrender.com:5004/session/${gameCode}?username=${username}`
+      `wss://bbd-lasertag.onrender.com/session/${gameCode}?username=${username}`
     );
     socketRef.current = socket;
 
