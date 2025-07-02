de8026ca69196998fca3716d1e6a36e02abdca2b
Nathan Wells <nthnwells1@gmail.com>
2025-07-02 13:11:51 +0200

    Fix opening new socket when going to camerview

--- client/src/pages/PlayerLobby.js
+++ client/src/pages/PlayerLobby.js
@@ -50,7 +50,7 @@ export default function PlayerLobby() {
           state: {
             username,
             gameCode,
-            color: currentPlayer?.codeId,
+            codeId: currentPlayer?.codeId,
           },
         });
       }
